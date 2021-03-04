9f0fc4cda3614d76578c268accc285539a80ef9d
17336293 <71881200+17336293@users.noreply.github.com>
2021-03-04 08:59:46 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,45 +1,56 @@
-"""Import Packages"""
-from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-import mysql.connector
-import credentials
 import requests
 import json
+import simplejson as json
+from pprint import pprint
+from IPython.display import JSON
+import sqlalchemy as sqla
+from sqlalchemy import create_engine, Column, Table, Integer, Float, String, MetaData, DateTime
+import traceback
+import glob
+import os
 import time
 import datetime
-import traceback
-import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import credentials
 
+#main function with functions of saving data and running data into Workbench running together
 def main():
-    """Create inital database if not already present"""
-    create_stations()
-    create_availability()
-    create_weather()
     
-    """create infinate loop where database updates every 5 minutes"""
+    try:
+        """information required to access api"""
+        apikey = "86e256748a1872c27d81d7f54516d214b19faa8e"
+        name = "Dublin"
+        url= "https://api.jcdecaux.com/vls/v1/stations"
+        r = requests.get(url, params={"apiKey":apikey,"contract": name})
+        data = json.loads(r.text)
+        
+        """Uplaod station data to database"""
+        store(r)
+        
+        """Store the inital data in Json file"""
+        current_time = datetime.datetime.now()
+        filename = 'data_{}.json'.format(current_time).replace(" ","_").replace(":","_")
+        store_data_json(data,filename)
+        
+    except:
+        print(traceback.format_exc())
+        
     while True:
         try:
-        
             """information required to access api"""
-            url = "https://api.jcdecaux.com/vls/v1/stations"
+            apikey = "86e256748a1872c27d81d7f54516d214b19faa8e"
             name = "Dublin"
-            apikey = "f570f2a1ccb689ea7b7ba8a4f9129743ed853448"
-            current_time = datetime.datetime.now()
-            filename = 'data_{}.json'.format(current_time).replace(" ","_").replace(":","_")
+            url= "https://api.jcdecaux.com/vls/v1/stations"
+
 
             r = requests.get(url, params={"apiKey":apikey,"contract": name})
 
             """update databases and store information"""
             data = json.loads(r.text)
-            store_data(data,filename)
-            stations_to_db(data)
-        
-            availabilty_to_db(data)
-            
-            weather_data = get_weather()
-            weather_to_db(weather_data)
+
+            # write to the database
+            store2(data)
         
             time.sleep(5*60)
         
@@ -48,131 +59,146 @@ def main():
         
     return
 
-def get_weather():
-    '''Uses open weather API to get current weather'''
-    url = 'http://api.openweathermap.org/data/2.5/weather?q=Rathfarnham&appid=59134ab26e3f2ded62e1e2b6e3c08c21'
-
-    r = requests.get(url)
-
-    weather_data = r.json()
-    
-    return weather_data 
-
-def store_data(data,filename):
+def store_data_json(data,filename):
     '''uploads the data to a json file called data_[X] where X is the time the json file was pulled from the API'''
     with open(filename,'w') as f:
         f.write(str(data))
     return
 
-"""Functions"""
-def stations_to_db(stations):
-    """update the stations table and populate it with """
-    print(type(stations),len(stations))
-    for station in stations:
-        print(station)
-        vals = (station.get('address'),int(station.get('banking')),station.get('bike_stands'),int(station.get('bonus')),
-                station.get('contract_name'),station.get('name'),station.get('number'),station.get('position').get('lat'),
-                station.get('position').get('lng'),station.get('status'))
-        engine.execute("insert into station values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", vals)
-    return
+"""information required to access api"""
+apikey = "86e256748a1872c27d81d7f54516d214b19faa8e"
+name = "Dublin"
+url= "https://api.jcdecaux.com/vls/v1/stations"
+r = requests.get(url, params={"apiKey":apikey,"contract": name})
 
-def weather_to_db(data):
-    print(data)
-    engine.execute("DELETE FROM weather")
-    vals = (data['weather'][0]['id'],data['weather'][0]['main'],data['weather'][0]['description'],
-            data['main']['temp_max'],data['main']['temp_min'],data['main']['temp'],data['main']['pressure'],
-            data['main']['humidity'])
-    engine.execute("insert into weather values(%s,%s,%s,%s,%s,%s,%s,%s)", vals)
-    return
+#returns values in sqlachemy object for static table
+def get_station(obj):
+    return{'number': obj['number'],
+           'name': obj['name'],
+           'address': obj['address'],
+           'pos_lng': obj['position']['lng'],
+           'pos_lat': obj['position']['lat'],
+           'bike_stands': obj['bike_stands']}
 
-def availabilty_to_db(available):
-    """empty the contents of the availability table and repopulate it with updated values"""
-    print(type(available),len(available))
-    engine.execute("DELETE FROM availability")
-    for entry in available:
-        print(entry)
-        vals = (entry.get('number'),int(entry.get('available_bikes')),int(entry.get('available_bike_stands')),entry.get('last_update'))
-        engine.execute("insert into availability values(%s,%s,%s,%s)", vals)
-    return
+#returns values in sqlachemy object for static table
+def get_availability(obj):
+    return {'number': obj['number'],
+            'bike_stands': obj['bike_stands'],
+            'available_bike_stands': obj['available_bike_stands'],
+            'available_bikes': obj['available_bikes'],
+            'last_update': datetime.datetime.now()}
 
-"""Main body of code"""
+def get_weather(obj):
+    return {'id': obj['weather'][0]['id'],
+            'description': obj['weather'][0]['main'],
+            'temp': obj['main']['temp'],
+            'temp_max': obj['main']['temp_max'],
+            'temp_min': obj['main']['temp_min'],
+            'pressure': obj['main']['pressure'],
+            'humidity': obj['main']['humidity'],
+            'last_update': datetime.datetime.now()}
 
-"""Import Credentails"""
-URI = "dublinbikeappdb.cxaxe40vwlui.us-east-1.rds.amazonaws.com"
-DB = "dbikes1"
-name = credentials.name
-pw = credentials.password
 
-"""Initate connection"""
-engine = create_engine("mysql+mysqlconnector://{}:{}@{}:3306/{}".format(name,pw,URI,DB),echo=True)
+def weather_data():
+    '''Uses open weather API to get current weather'''
+    url = 'http://api.openweathermap.org/data/2.5/weather?q=Rathfarnham&appid=59134ab26e3f2ded62e1e2b6e3c08c21'
+
+    r = requests.get(url)
+    weather_data = r.json()
     
-connection=engine.connect()
+    """store weather data in json file"""
+    current_time = datetime.datetime.now()
+    filename = 'weather_data_{}.json'.format(current_time).replace(" ","_").replace(":","_")
+    store_data_json(weather_data,filename)
+    
+    return weather_data 
 
-engine.execute("CREATE DATABASE IF NOT EXISTS dbikes1")
-R = engine.execute("SHOW DATABASES")
-print(R)
-
-def create_stations():
-    """Create Stations Table if none exists"""
-    sql = """
-    CREATE TABLE IF NOT EXISTS station(
-        address VARCHAR(256),
-        banking INTEGER,
-        bike_stands INTEGER,
-        bonus INTEGER,
-        contract_name VARCHAR(256),
-        name VARCHAR(256),
-        number INTEGER,
-        position_lat REAL,
-        position_lng REAL,
-        status VARCHAR(256)
-        )"""
-
+#function to execute storage of data into db for static table
+def store(data):
     try:
-        res = engine.execute("DROP TABLE IF EXISTS station")
-        res = engine.execute(sql)
-        print(res.fetchall())
+        URI = "dublinbikeappdb.cxaxe40vwlui.us-east-1.rds.amazonaws.com"
+        DB = "dbikes1"
+        name = credentials.name
+        pw = credentials.password
 
-    except Exception as e:
-        print(e)
-    
-def create_availability():
-    """Create avaialbeilty table if none exists"""
-    sql = """
-    CREATE TABLE IF NOT EXISTS availability (
-        number INTEGER,
-        available_bikes INTEGER,
-        available_bike_stands INTEGER,
-        last_update INTEGER
-        )"""
-    
-    try:
-        res = engine.execute(sql)
-        print(res.fetchall())
+        """Initate connection"""
+        engine = create_engine("mysql+mysqlconnector://{}:{}@{}:3306/{}".format(name,pw,URI,DB),echo=True)
+        dbikes = "dbikes1"
+        sql = """CREATE DATABASE IF NOT EXISTS %s;""" % (dbikes)
+        engine.execute(sql)
         
-    except Exception as e:
-        print(e)
+        '''Populate stations table'''
+        drop = """DROP TABLE IF EXISTS stations"""
+        engine.execute(drop)
+        
+        metadata.create_all(engine)
+        values = list(map(get_station, r.json()))
+        
+        engine.execute(stations.insert().values(values))
+       
+    except:
+        print(traceback.format_exc())
 
-def create_weather():
-    """Create weather table if none exists"""
-    sql = """
-    CREATE TABLE IF NOT EXISTS weather (
-        id INTEGER,
-        main VARCHAR(256),
-        description VARCHAR(256),
-        temp REAL,
-        temp_max REAL,
-        temp_min REAL,
-        pressure INTEGER,
-        humidity INTEGER
-        )"""
-
+#function to execute storage of data into db for static table
+def store2(data):
     try:
-        res = engine.execute(sql)
-        print(res.fetchall())
+        '''Store availability data'''
+        URI = "dublinbikeappdb.cxaxe40vwlui.us-east-1.rds.amazonaws.com"
+        DB = "dbikes1"
+        name = credentials.name
+        pw = credentials.password
 
-    except Exception as e:
-        print(e)
+        """Initate connection"""
+        engine = create_engine("mysql+mysqlconnector://{}:{}@{}:3306/{}".format(name,pw,URI,DB),echo=True)
+        #drop = """DROP TABLE IF EXISTS availability"""
+        engine.execute("DELETE FROM availability")
+        
+        '''Insert values from json file into availability table'''
+        metadata.create_all(engine)
+        values2 = list(map(get_availability, r.json()))
+        engine.execute(available.insert().values(values2))
+        
+        '''Store weather data'''
+        engine = create_engine("mysql+mysqlconnector://{}:{}@{}:3306/{}".format(name,pw,URI,DB),echo=True)
+        engine.execute(weather.insert().values(get_weather(weather_data())))
+        
+        
+    except:
+        print(traceback.format_exc())
+    
 
-"""Run the database updates"""
+'''Generate tables'''
+metadata = sqla.MetaData()
+
+'''Station table'''
+stations = sqla.Table(
+        'stations', metadata,
+        sqla.Column('number', sqla.Integer, primary_key = True),
+        sqla.Column('name', sqla.String(128)),
+        sqla.Column('address', sqla.String(128)),
+        sqla.Column('pos_lat', sqla.Float),
+        sqla.Column('pos_lng', sqla.Float),
+        sqla.Column('bike_stands', sqla.Integer))
+
+'''Availability table'''
+available = sqla.Table(
+        'availability', metadata,
+        sqla.Column('number', sqla.Integer),
+        sqla.Column('bike_stands', sqla.Integer),
+        sqla.Column('available_bike_stands', sqla.Integer),
+        sqla.Column('available_bikes', sqla.Integer),
+        sqla.Column('last_update', sqla.DateTime))
+
+'''Weather table'''
+weather = sqla.Table(
+    'weather',metadata,
+    sqla.Column('id', sqla.Integer),
+    sqla.Column('description',sqla.String(128)),
+    sqla.Column('temp',sqla.REAL),
+    sqla.Column('temp_max', sqla.REAL),
+    sqla.Column('temp_min', sqla.REAL),
+    sqla.Column('pressure', sqla.Integer),
+    sqla.Column('humidity', sqla.Integer),
+    sqla.Column('last_update', sqla.DateTime))
+     
 main()
