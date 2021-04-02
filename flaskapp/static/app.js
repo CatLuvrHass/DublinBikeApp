344c9d200be2a5549b162d232f475210fd08081f
hassan <hassan.albujasim@ucdconnect.ie>
2021-04-02 17:38:43 +0100

    included liveLocation to dropdown menu&CalRoute to chosen station&Route from station to station

--- flaskapp/static/app.js
+++ flaskapp/static/app.js
@@ -1,13 +1,24 @@
 let map;
-let another;
-function initMap() {
 
-    // const directionsService = new google.maps.DirectionsService();
-    // const directionsRenderer = new google.maps.DirectionsRenderer();
+// const locationInfo = document.querySelector('#location-info');
+//
+// function getLocation(){
+//     navigator.geolocation.getCurrentPosition(
+//         (position) => {
+//             const pos = {
+//                 lat: position.coords.latitude,
+//                 lng: position.coords.longitude,
+//             };
+//             document.getElementById("start").innerHTML = [pos.lat, pos.lng]
+//             document.getElementById("end").innerHTML = [pos.lat, pos.lng]
+//         });
+//
+// }
+
+function initMap() {
 
     fetch("/stations").then(response => {
         return response.json();
-
 
     }).then(data => {
         // console.log("data: ", data);
@@ -16,6 +27,22 @@ function initMap() {
       center: { lat: 53.349804, lng: -6.260310},
       zoom: 14,
     });
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+            const pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+            };
+            var originDropDown = document.getElementById("start");
+            var opt = document.createElement("option");
+
+            opt.value= [pos.lat, pos.lng];
+            opt.innerHTML = "mee";
+
+            originDropDown.add(opt);
+            console.log(pos.lng);
+        });
+
     const directionsService = new google.maps.DirectionsService();
     const directionsRenderer = new google.maps.DirectionsRenderer();
     directionsRenderer.setMap(map);
@@ -25,7 +52,7 @@ function initMap() {
     };
     document.getElementById("start").addEventListener("change", onChangeHandler);
     document.getElementById("end").addEventListener("change", onChangeHandler);
-
+    citySelect();
 
     data.forEach(stations => {
         const marker = new google.maps.Marker({
@@ -47,8 +74,6 @@ function initMap() {
 
     });
 
-    citySelect();
-
     }).catch(err => {
       console.log("OOPS!", err);
     });
@@ -64,7 +89,7 @@ function calculateAndDisplayRoute(directionsService, directionsRenderer) {
       destination: {
         query: document.getElementById("end").value,
       },
-      travelMode: google.maps.TravelMode.DRIVING,
+      travelMode: google.maps.TravelMode.BICYCLING,
     },
     (response, status) => {
       if (status === "OK") {
@@ -85,18 +110,20 @@ function citySelect(){
     }).then(another => {
         // console.log("anotherone: ", another);
             another.forEach(station =>{
-                const post_lat = station.pos_lat;
-                const post_lng = station.pos_lng;
-                const stationLatLng = { lat: post_lat, lng: post_lng };
-                console.log(stationLatLng);
-                start_ele.innerHTML = start_ele.innerHTML +
-                '<option value="' + station.name + ", Dublin, Ireland" + '">' + station.name + '</option>';
-                end_ele.innerHTML = end_ele.innerHTML +
-                '<option value="' +  station.name+ ", Dublin, Ireland" + '">' + station.name + '</option>';
-                const selectedStation = document.getElementById("start");
-                console.log(selectedStation);
-        //
-        // console.log(station.name);
+
+                var originDropDown = document.getElementById("start");
+                var opt = document.createElement("option");
+                opt.value= [station.pos_lat,station.pos_lng];
+                opt.innerHTML = station.name;
+
+                originDropDown.add(opt);
+
+                var originDropDown = document.getElementById("end");
+                var opt = document.createElement("option");
+                opt.value= [station.pos_lat,station.pos_lng];
+                opt.innerHTML = station.name;
+
+                originDropDown.add(opt);
     });
     });
 }
